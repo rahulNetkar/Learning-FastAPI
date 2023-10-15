638b0b60b27507ae91dcae29b84531edf8debe86
Rahul Netkar <rsn9.rn@gmail.com>
2023-10-15 22:48:10 +0530

    idk what I added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, status, HTTPException, Response
+
+# from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
+
 
 app = FastAPI()
 
@@ -9,21 +12,79 @@ app = FastAPI()
 class Post(BaseModel):
     title: str
     content: str
-    published: bool = True
+    # published: bool = True
     ratings: Optional[int] = None
 
 
+my_posts = [
+    {
+        "title": "first post",
+        "content": "first post content",
+        "ratings": 4,
+        "id": 1,
+    },
+    {
+        "title": "second post",
+        "content": "second post content",
+        "ratings": 3,
+        "id": 2,
+    },
+]
+
+
+def fetch(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
+def delete_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello, world!"}
 
 
-@app.get("/models/{item_id}")
-async def read_item(item_id: int):
-    return {"model_id": item_id}
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
 
 
-@app.post("/posts")
-async def create_post(post: Post):
-    print(post.dict())
-    return {"data": post}
+@app.get("/posts/{id}")
+def get_post_by_id(id: int):
+    post = fetch(id)
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id {id} not found.",
+        )
+
+    return {"post": post}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    p = post.dict()
+    p["id"] = randrange(0, 1000000)
+    my_posts.append(p)
+    return {"data": p}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_posts(id: int):
+    index = delete_post(id)
+
+    my_posts.pop(index)
+
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} not found",
+        )
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
